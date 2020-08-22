107025b79e1748d30ce1a9a167ea85783c75edea
Xavierdarkness <mrb1105@gmail.com>
2020-08-21 22:20:09 -0400

    -Inital Commit

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    //configuration paramaters;
+    [SerializeField] float screenWidthinUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosIntUnits = Input.mousePosition.x / Screen.width * screenWidthinUnits;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y) ;
+        paddlePos.x = Mathf.Clamp(mousePosIntUnits, minX, maxX);
+        transform.position = paddlePos;
+    }
+}
